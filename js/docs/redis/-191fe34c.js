3308392146bf91ba83c356a8691ebf67b8c9c998
fandong <871119239@qq.com>
2019-08-04 19:51:44 +0800

    third

---
+++ js/docs/redis/-191fe34c.js
@@ -0,0 +1,428 @@
+System.register([], function(__WEBPACK_DYNAMIC_EXPORT__) {
+
+	return {
+
+		execute: function() {
+			__WEBPACK_DYNAMIC_EXPORT__(
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/pages/md/db_redis_基础_1-1_概述.md");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./build/md-loader/index.js!./src/pages/md/db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d&":
+/*!*****************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??ref--5-0!./build/md-loader!./src/pages/md/db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d& ***!
+  \*****************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _vm._m(0)
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "content" }, [
+      _c(
+        "h1",
+        { attrs: { id: "redis-%E5%8F%91%E5%B8%83%E8%AE%A2%E9%98%85" } },
+        [
+          _c(
+            "a",
+            {
+              staticClass: "header-anchor",
+              attrs: {
+                href: "#redis-%E5%8F%91%E5%B8%83%E8%AE%A2%E9%98%85",
+                "aria-hidden": "true"
+              }
+            },
+            [_vm._v("#")]
+          ),
+          _vm._v(" Redis 发布订阅")
+        ]
+      ),
+      _vm._v(" "),
+      _c("p", [
+        _vm._v(
+          "Redis 发布订阅(pub/sub)是一种消息通信模式：发送者(pub)发送消息，订阅者(sub)接收消息。\nRedis 客户端可以订阅任意数量的频道。"
+        )
+      ]),
+      _vm._v(" "),
+      _c("p", [
+        _c("img", {
+          attrs: {
+            src:
+              "http://fandong90.github.io/dist/static/img/redissubscriber1.png",
+            alt: "subscribe"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("p", [_vm._v("新消息通过publish 推送给频道，订阅的接收消息")]),
+      _vm._v(" "),
+      _c("p", [
+        _c("img", {
+          attrs: {
+            src:
+              "http://fandong90.github.io/dist/static/img/redissubscriber2.png",
+            alt: "subscribe2"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("h1", { attrs: { id: "%E8%A1%8C%E5%8A%A8" } }, [
+        _c(
+          "a",
+          {
+            staticClass: "header-anchor",
+            attrs: { href: "#%E8%A1%8C%E5%8A%A8", "aria-hidden": "true" }
+          },
+          [_vm._v("#")]
+        ),
+        _vm._v(" 行动")
+      ]),
+      _vm._v(" "),
+      _c("ul", [_c("li", [_vm._v("注册频道")])]),
+      _vm._v(" "),
+      _c("pre", { staticClass: "hljs" }, [
+        _c("code", [_vm._v("    SUBSCRIBE redisChat\n\n")])
+      ]),
+      _vm._v(" "),
+      _c("pre", { staticClass: "hljs" }, [
+        _c("code", [
+          _vm._v("    "),
+          _c("span", { staticClass: "hljs-comment" }, [_vm._v("//订阅")]),
+          _vm._v(
+            "\n    \n      psubscribe redisChat\n   \tReading messages... (press Ctrl-C to quit)\n   \t"
+          ),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("1")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [_vm._v('"psubscribe"')]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("2")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [_vm._v('"redisChat"')]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("3")]),
+          _vm._v(") (integer) "),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("1")]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("1")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [_vm._v('"pmessage"')]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("2")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [_vm._v('"redisChat"')]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("3")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [_vm._v('"redisChat"')]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("4")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [
+            _vm._v('"nihao ,redis"')
+          ]),
+          _vm._v("\n\n\n")
+        ])
+      ]),
+      _vm._v(" "),
+      _c("pre", { staticClass: "hljs" }, [
+        _c("code", [
+          _vm._v("     "),
+          _c("span", { staticClass: "hljs-comment" }, [_vm._v("//退订")]),
+          _vm._v("\n     \n     "),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("127.0")]),
+          _vm._v(".0.1:"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("6379")]),
+          _vm._v("> punsubscribe redisChat\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("1")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [
+            _vm._v('"punsubscribe"')
+          ]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("2")]),
+          _vm._v(") "),
+          _c("span", { staticClass: "hljs-string" }, [_vm._v('"redisChat"')]),
+          _vm._v("\n   \t"),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("3")]),
+          _vm._v(") (integer) "),
+          _c("span", { staticClass: "hljs-number" }, [_vm._v("0")]),
+          _vm._v("\n\n")
+        ])
+      ]),
+      _vm._v(" "),
+      _c("p", [
+        _vm._v(
+          '···\n// 发布消息\n127.0.0.1:6379> publish redisChat "nihao ,redis 3333333"\n(integer) 1\n···'
+        )
+      ])
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js":
+/*!********************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/runtime/componentNormalizer.js ***!
+  \********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return normalizeComponent; });
+/* globals __VUE_SSR_CONTEXT__ */
+
+// IMPORTANT: Do NOT use ES2015 features in this file (except for modules).
+// This module is a runtime utility for cleaner component module output and will
+// be included in the final webpack user bundle.
+
+function normalizeComponent (
+  scriptExports,
+  render,
+  staticRenderFns,
+  functionalTemplate,
+  injectStyles,
+  scopeId,
+  moduleIdentifier, /* server only */
+  shadowMode /* vue-cli only */
+) {
+  // Vue.extend constructor export interop
+  var options = typeof scriptExports === 'function'
+    ? scriptExports.options
+    : scriptExports
+
+  // render functions
+  if (render) {
+    options.render = render
+    options.staticRenderFns = staticRenderFns
+    options._compiled = true
+  }
+
+  // functional template
+  if (functionalTemplate) {
+    options.functional = true
+  }
+
+  // scopedId
+  if (scopeId) {
+    options._scopeId = 'data-v-' + scopeId
+  }
+
+  var hook
+  if (moduleIdentifier) { // server build
+    hook = function (context) {
+      // 2.3 injection
+      context =
+        context || // cached call
+        (this.$vnode && this.$vnode.ssrContext) || // stateful
+        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
+      // 2.2 with runInNewContext: true
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__
+      }
+      // inject component styles
+      if (injectStyles) {
+        injectStyles.call(this, context)
+      }
+      // register component module identifier for async chunk inferrence
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier)
+      }
+    }
+    // used by ssr in case component is cached and beforeCreate
+    // never gets called
+    options._ssrRegister = hook
+  } else if (injectStyles) {
+    hook = shadowMode
+      ? function () { injectStyles.call(this, this.$root.$options.shadowRoot) }
+      : injectStyles
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // for template-only hot-reload because in that case the render fn doesn't
+      // go through the normalizer
+      options._injectStyles = hook
+      // register for functioal component in vue file
+      var originalRender = options.render
+      options.render = function renderWithStyleInjection (h, context) {
+        hook.call(context)
+        return originalRender(h, context)
+      }
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate
+      options.beforeCreate = existing
+        ? [].concat(existing, hook)
+        : [hook]
+    }
+  }
+
+  return {
+    exports: scriptExports,
+    options: options
+  }
+}
+
+
+/***/ }),
+
+/***/ "./src/pages/md/db_redis_基础_1-1_概述.md":
+/*!********************************************!*\
+  !*** ./src/pages/md/db_redis_基础_1-1_概述.md ***!
+  \********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _db_redis_1_1_md_vue_type_template_id_03654b4d___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d& */ "./src/pages/md/db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
+  script,
+  _db_redis_1_1_md_vue_type_template_id_03654b4d___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _db_redis_1_1_md_vue_type_template_id_03654b4d___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "src/pages/md/db_redis_基础_1-1_概述.md"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./src/pages/md/db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d&":
+/*!***************************************************************************!*\
+  !*** ./src/pages/md/db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d& ***!
+  \***************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_ref_5_0_build_md_loader_index_js_db_redis_1_1_md_vue_type_template_id_03654b4d___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??ref--5-0!../../../build/md-loader!./db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./build/md-loader/index.js!./src/pages/md/db_redis_基础_1-1_概述.md?vue&type=template&id=03654b4d&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_ref_5_0_build_md_loader_index_js_db_redis_1_1_md_vue_type_template_id_03654b4d___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_ref_5_0_build_md_loader_index_js_db_redis_1_1_md_vue_type_template_id_03654b4d___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ })
+
+/******/ })
+			);
+		}
+	};
+});
+//# sourceMappingURL=-191fe34c.js.map
